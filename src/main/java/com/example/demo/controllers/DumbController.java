571ab02d8ead993327c52a996812b0f45fb60258
Kirill Kulikov <kirill.kulikov@thinkbiganalytics.com>
2017-11-08 14:00:23 +0000

    first

---
+++ src/main/java/com/example/demo/controllers/DumbController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controllers;
+
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static java.lang.Math.atan;
+import static java.lang.Math.tan;
+
+@RestController
+public class DumbController {
+
+    @Autowired
+    private MeterRegistry meter;
+
+    @GetMapping("/hello")
+    public Mono<String> handle() {
+        return Flux.range(0, 65536)
+                .map(this::calculate)
+                .flatMap(this::calculateOther)
+                .count().map(Object::toString);
+    }
+
+    @Timed("calculate.plain.timed")
+    @Scheduled
+    private String calculate(int n) {
+
+        meter.counter("calculate.plain.explicit").increment();
+
+        return String.valueOf(tan(atan(tan(atan(n)))));
+    }
+
+    @Timed("calculate.reactive.timed")
+    @Scheduled
+    private Mono<String> calculateOther(String n) {
+
+        meter.counter("calculate.reactive.explicit").increment();
+
+        return Mono.just(n + "111");
+    }
+}
